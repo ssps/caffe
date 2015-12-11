65439c9168f65a697b86c03bda0a2799e463b2e5
Henggang Cui at susitna <hengganc@h0.ubuntu14-8.biglearning.susitna.pdl.cmu.local>
2015-12-11 08:28:56 -0700

    commit before merging

--- examples/LRCN_activity_recognition/8parts/run_geeps_lstm_RGB.sh
+++ examples/LRCN_activity_recognition/8parts/run_geeps_lstm_RGB.sh
@@ -14,5 +14,4 @@ cp train_test_lstm_RGB.prototxt.template $1/.
 cp sequence_input_layer.py.template $1/.
 cp machinefile $1/.
 cp ps_config $1/.
-mpirun -machinefile machinefile ../../../build/tools/caffe_mpi train --solver=lstm_solver_RGB.prototxt --ps_config=ps_config --weights=../single_frame_all_layers_hyb_RGB_iter_5000.caffemodel
-# 2>&1 | tee $1/output.txt
+mpirun -machinefile machinefile ../../../build/tools/caffe_mpi train --solver=lstm_solver_RGB.prototxt --ps_config=ps_config --weights=../single_frame_all_layers_hyb_RGB_iter_5000.caffemodel 2>&1 | tee $1/output.txt
--- scripts/duplicate.py
+++ scripts/duplicate.py
@@ -7,13 +7,20 @@ num_dups = int(sys.argv[2])
 intab = '%'
 if len(sys.argv) > 3:
   intab = sys.argv[3]
+pattern = ''
+if len(sys.argv) > 4:
+  pattern = sys.argv[4]
 
 template_file = '%s.template' % (file_name)
 template_fd = open(template_file, 'r')
 
+output_files = []
 output_fds = []
 for i in range(num_dups):
   output_file = '%s.%i' % (file_name, i)
+  if pattern != '':
+    output_file = pattern % i
+  output_files.append(output_file)
   output_fds.append(open(output_file, 'w'))
 
 for line in template_fd:
@@ -28,6 +35,5 @@ template_fd.close()
 for i in range(num_dups):
   output_fds[i].close()
 
-for i in range(num_dups):
-  output_file = '%s.%i' % (file_name, i)
+for output_file in output_files:
   os.system('chmod a+x %s' % output_file)
