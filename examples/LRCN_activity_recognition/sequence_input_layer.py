ca6959256a331b2406912ea4908c902af7c02f25
Henggang Cui at susitna <hengganc@ops.susitna.pdl.cmu.local>
2016-01-13 16:25:01 -0500

    minor tweaks

--- examples/LRCN_activity_recognition/sequence_input_layer.py
+++ examples/LRCN_activity_recognition/sequence_input_layer.py
@@ -25,8 +25,8 @@ flow_frames = 'flow_images/'
 RGB_frames = 'frames/'
 test_frames = 16 
 train_frames = 16
-test_buffer = 3
-train_buffer = 8
+test_buffer = 4
+train_buffer = 4
 
 def processImageCrop(im_info, transformer, flow):
   im_path = im_info[0]
