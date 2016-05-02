cb39805324de98af7f8776d2e6e03a2d8d9409d0
Henggang Cui at susitna <hengganc@h0.ubuntu14-8-2.biglearning.susitna.pdl.cmu.local>
2016-05-02 16:04:00 -0600

    making cifar10 work with pdsh

---
+++ examples/cifar10/8parts/train_inception.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+python scripts/duplicate.py examples/cifar10/8parts/inception_train_val.prototxt 8
+python scripts/duplicate.py examples/cifar10/8parts/inception_solver.prototxt 8
+pdsh -R ssh -w ^examples/cifar10/8parts/machinefile "cd $(pwd) && ./build/tools/caffe_geeps train --solver=examples/cifar10/8parts/inception_solver.prototxt --ps_config=examples/cifar10/8parts/ps_config --worker_id=%n"
